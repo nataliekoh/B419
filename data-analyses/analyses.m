9cf78d0104e025c6c1640c17ea3599209c0f7724
Vincent Wu <wuv21@uw.edu>
2016-02-23 21:23:22 -0800

    removed extra files and improved instructions in analyses.m

--- data-analyses/analyses.m
+++ data-analyses/analyses.m
@@ -1,24 +1,26 @@
 %% load the data
-file = readtable('all_condensed.csv');
+
+% Make sure working directory is set as B419 folder.
+file = readtable('data/all_condensed_v3.csv');
 
 %% basic descriptions
 % col = desc
-% 2 = year
-% 3 = # of TB
-% 4 = % of TB patients with known HIV status
-% 5 = % of TB patients (tested) that are HIV-positive
-% 6 = % of HIV + TB patients on CPT
-% 7 = % of HIV + TB patients on ART
+% 1 = year
+% 2 = # of TB
+% 3 = % of TB patients with known HIV status
+% 4 = % of TB patients (tested) that are HIV-positive
+% 5 = % of HIV + TB patients on CPT
+% 6 = % of HIV + TB patients on ART
 
 
 % todo - merge cote d'ivoire
 unique_countries = numel(unique(file(:, 2)));
 
 % view afghanistan # of TB
-afghan_i = find(strcmp(file{:, 2}, 'Afghanistan') == 1);
+afghan_i = find(strcmp(file{:, 1}, 'Afghanistan') == 1);
 
-afghan_years = file{afghan_i, 3};
-afghan_nTB = file{afghan_i, 4};
+afghan_years = file{afghan_i, 2};
+afghan_nTB = file{afghan_i, 3};
 
 figure;
 plot(afghan_years, afghan_nTB);
