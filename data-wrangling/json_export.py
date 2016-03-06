86d517e56fbcd140db4240a5a29d8e417686f60a
Vincent Wu <wuv21@uw.edu>
2016-03-06 11:46:55 -0800

    changed d3 choropleth library to better support legends + modified csv methods and files needed to generate new data for visualization

--- data-wrangling/json_export.py
+++ data-wrangling/json_export.py
@@ -9,14 +9,33 @@ with open('../data/all_condensed_v7.csv', 'r') as csvfile:
     for row in reader:
         data.append(row)
 
-data_2010 = {}
+# data_2010 = {}
+# for i in range(1, len(data)):
+#     if data[i][2] == '2010':
+#         try:
+#             tb_prev = float(data[i][3]) / (float(data[i][24]) * 1000) * 100
+#             data_2010[data[i][1]] = tb_prev
+#         except ValueError as e:
+#             print(str(e) + ' at row ' + str(i))
+
+# writes to json
+# with open('tb_prev_2010.json', 'w') as output:
+#     json.dump(data_2010, output)
+
+year = '2005'
+
+data_proccesed = []
 for i in range(1, len(data)):
-    if data[i][2] == '2010':
+    if data[i][2] == year:
         try:
-            tb_prev = float(data[i][3]) / (float(data[i][24]) * 1000) * 100
-            data_2010[data[i][1]] = tb_prev
+            tb_prev = float(data[i][3]) / (float(data[i][24]) * 1000)
+            data_proccesed.append([data[i][1], tb_prev])
         except ValueError as e:
             print(str(e) + ' at row ' + str(i))
 
-with open('tb_prev_2010.json', 'w') as output:
-    json.dump(data_2010, output)
+data_proccesed.insert(0, ['country code', 'prev_' + year])
+
+# writes to csv
+with open('tb_prev_' + year + '.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerows(data_proccesed)
--- website/js/script.js
+++ website/js/script.js
@@ -1,59 +1,25 @@
 $(document).ready(function() {
    'use strict';
 
-    $.getJSON('data/tb_prev_2010.json', function(data) {
-        var minValue = 100;
-        var maxValue = 0;
-        for (var key in data) {
-            if (data[key] > maxValue) {
-                maxValue = data[key];
-            } else if (data[key] < minValue) {
-                minValue = data[key];
-            }
-        }
+    var format = function(d) {
+        return d3.format('.03%')(d);
+    };
 
-        var paletteScale = d3.scale.linear()
-            .domain([minValue, maxValue])
-            .range(['#fee0d2', '#67000d']);
+    var prev_years = [2005, 2010];
 
-        var coloredData = {};
-        for (var key in data) {
-            var prevValue = Math.round(data[key] * 1000) / 1000;
-            coloredData[key] = {
-                prev: prevValue,
-                fillColor: paletteScale(prevValue)
-            };
-        }
+    prev_years.forEach(function(year) {
+        console.log(year);
+        var map = d3.geomap.choropleth()
+            .geofile('d3-geomap/topojson/world/countries.json')
+            .column('Prevalence (' + year + ')')
+            .format(format)
+            .legend(true)
+            .unitId('country code');
 
-        var map = new Datamap({
-            element: document.getElementById('prev_map'),
-            fills: {
-                defaultFill: '#CCC'
-            },
-            data: coloredData,
-            geographyConfig: {
-                highlightFillColor: function(geo) {
-                    return geo['fillColor'] || '#74c476';
-                },
-                highlightBorderColor: '#74c476',
-                popupTemplate: function(geo, data) {
-                    if (!data) {
-                        return ['<div class="hoverinfo">',
-                            '<strong>', geo.properties.name, '</strong>',
-                            '<br>Count: <strong>', 'N/A', '</strong>',
-                            '</div>'].join('');
-                    }
-
-                    return ['<div class="hoverinfo">',
-                        '<strong>', geo.properties.name, '</strong>',
-                        '<br>Prevalence: <strong>', data.prev, '%</strong>',
-                        '</div>'].join('');
-                }
-            }
+        d3.csv('data/tb_prev_' + year + '.csv', function(error, data) {
+            d3.select('#prev_map_' + year)
+                .datum(data)
+                .call(map.draw, map);
         });
-
-        map.legend();
     });
-
-
 });
